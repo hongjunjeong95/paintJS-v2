5a821666e9ff7bbaf4cd11cca63b5a88e1fc9756
zpskek <wjdghdwns0@gmail.com>
2020-09-15 21:07:02 +0900

    Filling the canvas

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,14 @@ const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
 const boldRange = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
 const INITIAL_LINE_WIDTH = 2.5;
 
-let paint = false;
+let painting = false;
+let filling = false;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
@@ -18,7 +20,7 @@ ctx.strokeStyle = INITIAL_COLOR;
 function handleMousemove(e) {
   x = e.offsetX;
   y = e.offsetY;
-  if (!paint) {
+  if (!painting) {
     ctx.beginPath();
     ctx.moveTo(x, y);
   } else {
@@ -28,7 +30,7 @@ function handleMousemove(e) {
 }
 
 function startPaint() {
-  paint = true;
+  painting = true;
 }
 
 function handleMouseDown() {
@@ -36,7 +38,7 @@ function handleMouseDown() {
 }
 
 function stopPaint() {
-  paint = false;
+  painting = false;
 }
 
 function handleMouseUp() {
@@ -46,6 +48,7 @@ function handleMouseUp() {
 function handleClickColor(e) {
   const color = e.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleInputRange(e) {
@@ -53,14 +56,30 @@ function handleInputRange(e) {
   ctx.lineWidth = size;
 }
 
+function handleClickFill() {
+  if (filling) ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function handleClickMode(e) {
+  if (!filling) {
+    mode.innerHTML = 'Paint';
+    filling = true;
+  } else {
+    mode.innerHTML = 'Fill';
+    filling = false;
+  }
+}
+
 if (canvas) {
   canvas.addEventListener('mousemove', handleMousemove);
   canvas.addEventListener('mousedown', handleMouseDown);
   canvas.addEventListener('mouseup', handleMouseUp);
+  canvas.addEventListener('click', handleClickFill);
 
   Array.from(colors).forEach((color) =>
     color.addEventListener('click', handleClickColor)
   );
 
   boldRange.addEventListener('input', handleInputRange);
+  mode.addEventListener('click', handleClickMode);
 }
